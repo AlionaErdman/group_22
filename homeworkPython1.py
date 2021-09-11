8ab86710fdcd189e8a41918904f3c323f479af2c
Aliona Erdman <aliona_erdman@mail.ru>
2021-09-11 23:34:07 +0300

    hw3_python

---
+++ homeworkPython1.py
@@ -0,0 +1,53 @@
+str = ('Hello')
+print(str)
+print(type(str))
+int = 10
+print(int)
+print(type(int))
+float = 3.1415926
+print(float)
+print(type(float))
+bytes({46, 46, 46})
+print(bytes)
+print(type(bytes))
+list = {1, 2, 3}
+print(list)
+print(type(list))
+a = tuple('Hello, world!')
+print(a)
+print(type(tuple))
+a = set('hello')
+print(a)
+print(type(a))
+a = {'a', 'b', 'c', 'd'}
+print(a)
+print(type(a))
+b = frozenset('qwerty')
+print(b)
+print(type(b))
+d1 = dict()
+print(type(d1))
+param_1 = 'Hello'
+param_2 = 'world'
+param_3 = param_1 + param_2
+print(param_3)
+param_1 = 5
+param_2 = 10
+param_3 = param_1 + param_2
+print(param_3)
+param_1 = 5
+param_2 = 10
+param_3 = param_2 / param_1
+print(param_3)
+param_1 = 5
+param_2 = 10
+param_3 = param_2 * param_1
+print(param_3)
+param_1 = 5
+param_2 = 10
+param_3 = param_2 // param_1
+print(param_3)
+param_1 = 5
+param_2 = 10
+param_3 = param_2 % param_1
+print(param_3)
